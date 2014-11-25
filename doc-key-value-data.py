fc951bff16b5f532bed493c74a59cddedc44867a
Chris Keller <christopherlawrencekeller@gmail.com>
2014-11-25 11:55:48 -0800

    formats and updates to notes and key-value data script

--- doc-key-value-data.py
+++ doc-key-value-data.py
@@ -1,22 +1,33 @@
-# doc-key-value-data.py
-# uses http://datadesk.github.com/python-documentcloud/
-# pulls key-value data associated with a document referenced in the list
+"""
+file: doc-key-value-data.py
+what: given a project id returns the key/value data for a document
+uses: http://datadesk.github.com/python-documentcloud/
+more: https://www.documentcloud.org/help/searching
+"""
 
+# import the modules for this script
 from documentcloud import DocumentCloud
 from docConfig import config_settings
 
-# authenticate with document cloud
-# set user_name & password in docConfig.py
-client = DocumentCloud(config_settings['user_name'], config_settings['password'])
+# varible to hold the project we're targeting
+MY_PROJECT_ID = 123345
 
-# target documents set in docConfig.py
-document_list = config_settings['document_list']
+# authenticate with document cloud with user_name & password in docConfig.py
+client = DocumentCloud(
+    config_settings['user_name'], config_settings['password']
+)
 
 # begin function to return key-value data
-def return_document_key_value_data():
+def return_document_key_value_data(project_id):
+
+    # creates an object that contains the documents in the project
+    project_object = client.projects.get(id=project_id)
+
+    # list to hold all of the documents ids
+    list_of_documents = project_object.document_ids
 
     # loop through the list
-    for document in document_list:
+    for document in list_of_documents:
 
         # get document in list
         obj = client.documents.get(document)
@@ -30,8 +41,6 @@ def return_document_key_value_data():
             # see what we're getting back
             print '%s: %s' % (type, value)
 
-        # end group of key-value for document
-        print 'Finished printing key-value data for %s\n' % (document)
-
-# run the function
-return_document_key_value_data()
+# runs the function specified
+if __name__ == "__main__":
+    return_document_key_value_data(MY_PROJECT_ID)
--- doc-notes.py
+++ doc-notes.py
@@ -1,22 +1,31 @@
-# doc-notes.py
-# uses http://datadesk.github.com/python-documentcloud/
-# searches documents for their notes, description and the pages they are on
+"""
+file: doc-notes.py
+what: searches documents for their notes, description and the pages they are on
+uses: http://datadesk.github.com/python-documentcloud/
+"""
 
 from documentcloud import DocumentCloud
 from docConfig import config_settings
 
-# authenticate with document cloud
-# set user_name & password in docConfig.py
-client = DocumentCloud(config_settings['user_name'], config_settings['password'])
+# varible to hold the project we're targeting
+MY_PROJECT_ID = 123345
 
-# target documents set in docConfig.py
-document_list = config_settings['document_list']
+# authenticate with document cloud with user_name & password in docConfig.py
+client = DocumentCloud(
+    config_settings['user_name'], config_settings['password']
+)
 
 # begin function to return notes
-def return_document_notes():
+def return_document_notes(project_id):
+
+    # creates an object that contains the documents in the project
+    project_object = client.projects.get(id=project_id)
+
+    # list to hold all of the documents ids
+    list_of_documents = project_object.document_ids
 
     # loop through the list
-    for document in document_list:
+    for document in list_of_documents:
 
         # Fetch using the id
         obj = client.documents.get(document)
@@ -28,12 +37,9 @@ def return_document_notes():
         for note in document_notes:
             try:
                 print '%s: %s (%s) - (%s)' % (note.title, note.description, note.page, note.access)
-
             except:
                 print '%s (%s) - (%s)' % (note.title, note.page, note.access)
 
-        # end group of notes for document
-        print 'Finished printing notes for %s\n' % (document)
-
-# run the function
-return_document_notes()
+# runs the function specified
+if __name__ == "__main__":
+    return_document_notes(MY_PROJECT_ID)
